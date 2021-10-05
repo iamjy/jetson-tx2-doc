275a642c8dd29cfef2dc760005887fec38a0799b
Jinyoung Park <iamjy1005@gmail.com>
2021-10-05 14:36:29 +0900

    Create basic configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+#   'sphinx_rtd_theme' # Installing directly from the repository source is deprecated and is not recommended.
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +48,14 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The master toctree document.
+master_doc = 'index'
